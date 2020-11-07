4b1abb2eaf4dddd58160260987d524b58b17b909
Jason Gilmour <jason@stormideas.com>
2020-11-07 13:09:35 +0000

    Initial Commit

---
+++ Daily/DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyApp.swift
+//  Daily
+//
+//  Created by Jason Gilmour on 07/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
