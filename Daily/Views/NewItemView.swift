d6d292f3ff0255ae312db54a69ead93a212a741a
Jason Gilmour <jason@stormideas.com>
2020-12-29 15:06:28 +0000

    WIP on changing UX to use overlay sheet

--- Daily/Views/NewItemView.swift
+++ Daily/Views/NewItemView.swift
@@ -7,24 +7,33 @@
 
 import SwiftUI
 
-//struct NewItemView: View {
-//    @ObservedObject var store = HabitStore()
-//    @Binding var isPresented: Bool
-//    @State private var habit: String = ""
+struct NewItemView: View {
+    //    @ObservedObject var store = HabitStore()
+    @ObservedObject var taskListVM = TaskListViewModel()
+    @Binding var isPresented: Bool
+    @State private var habit: String = ""
+    
 //    var sections = ["Morning", "Afternoon", "Evening"]
 //    var frequencies = ["Daily", "Weekly", "Monthly"]
 //    @State private var selectedSectionIndes = 0
 //    @State private var selectedFrequencyIndex = 0
-//
-//    func addHabit() {
-//        store.habits.append(Habit(title: "Holy shit it worked", completedToday: false))
-//        self.isPresented = false
-//    }
-//
-//    var body: some View {
-//        NavigationView {
-//            Form {
-//                VStack(alignment: .leading) {
+    
+    //    func addHabit() {
+    //        store.habits.append(Habit(title: "Holy shit it worked", completedToday: false))
+    //        self.isPresented = false
+    //    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                VStack(alignment: .leading) {
+                    TaskCell(taskCellVM: TaskCellViewModel.newTask()) { result in
+                        if case .success(let task) = result {
+                            self.taskListVM.addTask(task: task)
+                        }
+                        self.isPresented.toggle()
+                    }
+                    
 //                    Section {
 //                        Text("Habit")
 //                            .font(.system(.caption, design: .serif))
@@ -36,95 +45,98 @@ import SwiftUI
 //                        Divider()
 //                            .padding(.bottom)
 //                    }
-////                    Section {
-////                        Picker(selection: self.$selectedCrossSection, label: Text("Querschnitt")) {
-////
-////                            ForEach(self.oListDatasource.CrossSections, id: \.self) { item in
-////                                Text(item.CrossSection).tag(item.rowIndex)
-////                            }
-////                        }
-////                            //.pickerStyle(SegmentedPickerStyle()).foregroundColor(Color.orange)
-////
-////                            .font(Font.custom("ArialMT", size: 12))
-////                            .onReceive([self.selectedCrossSection].publisher.first()) { (value) in
-////                                print(self.oListDatasource.CrossSections[self.selectedCrossSection].CrossSection)
-////
-////                        }
-////                    }
-//                    Section {
-//                        Picker(
-//                            selection: $selectedSectionIndes,
-//                            label: Text("Section")
-//                                .font(.system(.caption, design: .serif))
-//                                .tracking(2)
-//                                .foregroundColor(Color("TextMuted"))
-//                                .textCase(.uppercase)
-//                        ) {
-//                            ForEach(0 ..< sections.count) {
-//                                Text(self.sections[$0])
-//                                    .foregroundColor(Color("TextDefault"))
-//                            }
-//                        }
-//                        Divider()
-//                            .padding(.vertical)
-//                    }
-//                    Section {
-//                        Picker(
-//                            selection: $selectedFrequencyIndex,
-//                            label: Text("Frequency")
-//                                .font(.system(.caption, design: .serif))
-//                                .tracking(2)
-//                                .foregroundColor(Color("TextMuted"))
-//                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-//                        ) {
-//                            ForEach(0 ..< frequencies.count) {
-//                                Text(self.frequencies[$0])
-//                                    .foregroundColor(Color("TextDefault"))
-//                            }
-//                        }
-//                        .buttonStyle(BorderlessButtonStyle())
-//                        Divider()
-//                            .padding(.vertical)
-//                    }
-//                    Section {
-//                        VStack(alignment: .center) {
-//                            Button(action: addHabit, label: {
-//                                Text("Add")
-//                                    .font(.system(.title2, design: .serif))
-//                                    .fontWeight(.bold)
-//                                    .italic()
-//                                    .tracking(2)
-//                                    .foregroundColor(Color("TextInverted"))
-//                                    .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-//                                    .frame(width: 144, height: 44)
-//                                    .background(Color("AccentColor"))
-//                                    .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-//                            })
-//                            .buttonStyle(BorderlessButtonStyle())
-//                        }
-//                        .frame(maxWidth: .infinity)
-//                    }
-//                    Spacer()
-//                }
-//                .background(Color(.clear))
-//                .padding(.vertical)
-//            }
-//            .background(LinearGradient(
-//                gradient: Gradient(colors: [Color("BackgroundTop"), Color("BackgroundBottom")]),
-//                startPoint: .top,
-//                endPoint: .bottom
-//            ))
-//            .clipShape(RoundedRectangle(cornerRadius: 16))
-//            .font(.system(.body, design: .serif))
-//            .frame(maxWidth: 560)
-//            .padding()
-//            .navigationBarTitle("Build a Habit")
-//        }
-//    }
-//}
-//
-//struct NewItemView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        NewItemView(isPresented: .constant(true))
-//    }
-//}
+                    //                    Section {
+                    //                        Picker(selection: self.$selectedCrossSection, label: Text("Querschnitt")) {
+                    //
+                    //                            ForEach(self.oListDatasource.CrossSections, id: \.self) { item in
+                    //                                Text(item.CrossSection).tag(item.rowIndex)
+                    //                            }
+                    //                        }
+                    //                            //.pickerStyle(SegmentedPickerStyle()).foregroundColor(Color.orange)
+                    //
+                    //                            .font(Font.custom("ArialMT", size: 12))
+                    //                            .onReceive([self.selectedCrossSection].publisher.first()) { (value) in
+                    //                                print(self.oListDatasource.CrossSections[self.selectedCrossSection].CrossSection)
+                    //
+                    //                        }
+                    //                    }
+                    
+                    
+                    
+                    //                    Section {
+                    //                        Picker(
+                    //                            selection: $selectedSectionIndes,
+                    //                            label: Text("Section")
+                    //                                .font(.system(.caption, design: .serif))
+                    //                                .tracking(2)
+                    //                                .foregroundColor(Color("TextMuted"))
+                    //                                .textCase(.uppercase)
+                    //                        ) {
+                    //                            ForEach(0 ..< sections.count) {
+                    //                                Text(self.sections[$0])
+                    //                                    .foregroundColor(Color("TextDefault"))
+                    //                            }
+                    //                        }
+                    //                        Divider()
+                    //                            .padding(.vertical)
+                    //                    }
+                    //                    Section {
+                    //                        Picker(
+                    //                            selection: $selectedFrequencyIndex,
+                    //                            label: Text("Frequency")
+                    //                                .font(.system(.caption, design: .serif))
+                    //                                .tracking(2)
+                    //                                .foregroundColor(Color("TextMuted"))
+                    //                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    //                        ) {
+                    //                            ForEach(0 ..< frequencies.count) {
+                    //                                Text(self.frequencies[$0])
+                    //                                    .foregroundColor(Color("TextDefault"))
+                    //                            }
+                    //                        }
+                    //                        .buttonStyle(BorderlessButtonStyle())
+                    //                        Divider()
+                    //                            .padding(.vertical)
+                    //                    }
+                    Section {
+                        VStack(alignment: .center) {
+                            Button( action: {}, label: {
+                                Text("Add")
+                                    .font(.system(.title2, design: .serif))
+                                    .fontWeight(.bold)
+                                    .italic()
+                                    .tracking(2)
+                                    .foregroundColor(Color("TextInverted"))
+                                    .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                                    .frame(width: 144, height: 44)
+                                    .background(Color("AccentColor"))
+                                    .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                            })
+                            .buttonStyle(BorderlessButtonStyle())
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                    Spacer()
+                }
+                .background(Color(.clear))
+                .padding(.vertical)
+            }
+            .background(LinearGradient(
+                gradient: Gradient(colors: [Color("BackgroundTop"), Color("BackgroundBottom")]),
+                startPoint: .top,
+                endPoint: .bottom
+            ))
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .font(.system(.body, design: .serif))
+            .frame(maxWidth: 560)
+            .padding()
+            .navigationBarTitle("Build a Habit")
+        }
+    }
+}
+
+struct NewItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewItemView(isPresented: .constant(true))
+    }
+}
--- Daily/Views/TaskLiatView.swift
+++ Daily/Views/TaskLiatView.swift
@@ -9,77 +9,83 @@ import SwiftUI
 
 
 struct TaskListView: View {
-  @ObservedObject var taskListVM = TaskListViewModel()
-  @State var presentAddNewItem = false
-  
-  var body: some View {
-    NavigationView {
-      VStack(alignment: .leading) {
-        List {
-          ForEach (taskListVM.taskCellViewModels) { taskCellVM in
-            TaskCell(taskCellVM: taskCellVM)
-          }
-          .onDelete { indexSet in
-            self.taskListVM.removeTasks(atOffsets: indexSet)
-          }
-          if presentAddNewItem {
-            TaskCell(taskCellVM: TaskCellViewModel.newTask()) { result in
-              if case .success(let task) = result {
-                self.taskListVM.addTask(task: task)
-              }
-              self.presentAddNewItem.toggle()
+    @ObservedObject var taskListVM = TaskListViewModel()
+    @State var presentAddNewItem = false
+    @State var showNewItemForm = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Group() {
+                    Button(action: {}) {
+                        Text("Menu")
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                ScrollView(.vertical) {
+                    VStack {
+                        Text("— Morning —")
+                            .font(.system(.title, design: .serif))
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                            .italic()
+                            .padding(.vertical)
+                        Group {
+                            ForEach (taskListVM.taskCellViewModels) { taskCellVM in
+                                TaskCell(taskCellVM: taskCellVM)
+                            }
+                            //                        .onDelete { indexSet in
+                            //                            self.taskListVM.removeTasks(atOffsets: indexSet)
+                            //                        }
+                            if presentAddNewItem {
+                                TaskCell(taskCellVM: TaskCellViewModel.newTask()) { result in
+                                    if case .success(let task) = result {
+                                        self.taskListVM.addTask(task: task)
+                                    }
+                                    self.presentAddNewItem.toggle()
+                                }
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: { self.showNewItemForm.toggle() }) {
+                        Text("Form your first habit")
+                    }
+                    .padding()
+                    .sheet(isPresented: $showNewItemForm) {
+                        NewItemView(isPresented: self.$showNewItemForm)
+                    }
+                }
             }
-          }
+            .foregroundColor(Color("TextDefault"))
+            .background(LinearGradient(
+                gradient: Gradient(colors: [Color("BackgroundTop"), Color("BackgroundBottom")]),
+                startPoint: .top,
+                endPoint: .bottom
+            ))
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .font(.system(.body, design: .serif))
+            .frame(maxWidth: .infinity)
+            .padding()
+            .navigationBarHidden(true)
+            //      .navigationBarTitle("Tasks")
         }
-        Button(action: { self.presentAddNewItem.toggle() }) {
-          HStack {
-            Image(systemName: "plus.circle.fill")
-              .resizable()
-              .frame(width: 20, height: 20)
-            Text("New Task")
-          }
-        }
-        .padding()
-        .accentColor(Color(UIColor.systemRed))
-      }
-      .navigationBarTitle("Tasks")
     }
-  }
 }
 
 struct TaskListView_Previews: PreviewProvider {
-  static var previews: some View {
-    TaskListView()
-  }
+    static var previews: some View {
+        TaskListView()
+    }
 }
 
 enum InputError: Error {
-  case empty
-}
-
-struct TaskCell: View {
-  @ObservedObject var taskCellVM: TaskCellViewModel
-  var onCommit: (Result<Task, InputError>) -> Void = { _ in }
-  
-  var body: some View {
-    HStack {
-      Image(systemName: taskCellVM.completionStateIconName)
-        .resizable()
-        .frame(width: 20, height: 20)
-        .onTapGesture {
-          self.taskCellVM.task.completed.toggle()
-        }
-      TextField("Enter task title", text: $taskCellVM.task.title,
-                onCommit: {
-                  if !self.taskCellVM.task.title.isEmpty {
-                    self.onCommit(.success(self.taskCellVM.task))
-                  }
-                  else {
-                    self.onCommit(.failure(.empty))
-                  }
-      }).id(taskCellVM.id)
-    }
-  }
+    case empty
 }
 
 //struct ContentView: View {
@@ -139,7 +145,36 @@ struct TaskCell: View {
 //        .padding()
 //    }
 //}
-//
+
+struct TaskCell: View {
+    @ObservedObject var taskCellVM: TaskCellViewModel
+    var onCommit: (Result<Task, InputError>) -> Void = { _ in }
+    
+    var body: some View {
+        HStack() {
+            Image(systemName: taskCellVM.completionStateIconName)
+                .resizable()
+                .foregroundColor(Color(taskCellVM.task.completed ? "AccentColor" : "TextMuted"))
+                .font(.system(size: 24, weight: taskCellVM.task.completed ? .medium : .light))
+                .frame(width: 32, height: 32)
+                .onTapGesture {
+                    self.taskCellVM.task.completed.toggle()
+                }
+            TextField("Enter task title", text: $taskCellVM.task.title,
+                      onCommit: {
+                        if !self.taskCellVM.task.title.isEmpty {
+                            self.onCommit(.success(self.taskCellVM.task))
+                        }
+                        else {
+                            self.onCommit(.failure(.empty))
+                        }
+                      }).id(taskCellVM.id)
+        }
+        .frame(alignment: .leading)
+        .padding(.vertical, 4)
+    }
+}
+
 //struct HabitItem: View {
 //    var title: String
 //    var completedToday: Bool
